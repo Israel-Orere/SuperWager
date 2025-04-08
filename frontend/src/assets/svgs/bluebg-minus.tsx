da7066088a2554bae3521677393715f781c254e2
dLuxKid <adetunjimarvellous09@gmail.com>
2025-04-08 08:55:14 +0100

    Revert "changed fe folder to next, ts and tailwind"
    This reverts commit 22fa8f7f7fdfa870f142b83f622f87a451e06f54.

---
+++ frontend/src/assets/svgs/bluebg-minus.tsx
@@ -0,0 +1,18 @@
+export default function MinusIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      {...props}
+      width="23"
+      height="23"
+      viewBox="0 0 23 23"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect width="22.189" height="22.189" fill="#0080FF" />
+      <path
+        d="M6.54947 9.86309H15.6128V11.9157H6.54947V9.86309Z"
+        fill="white"
+      />
+    </svg>
+  );
+}
---
+++ frontend/src/assets/svgs/play-button.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function PlayIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      {...props}
+      width="41"
+      height="39"
+      viewBox="0 0 41 39"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M35.6388 11.9919C41.86 14.8442 41.86 23.6838 35.6388 26.5361L11.8341 37.4502C6.53496 39.8798 0.5 36.0077 0.5 30.1781V8.34998C0.5 2.52038 6.53497 -1.35171 11.8341 1.07788L35.6388 11.9919Z"
+        fill="#0080FF"
+      />
+    </svg>
+  );
+}
